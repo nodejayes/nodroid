d2663822033d1bea2772cea43d138d07363296dd
Markus <markusgilg@outlook.de>
2017-11-19 19:53:44 +0100

    Init

---
+++ README.md
@@ -0,0 +1,83 @@
+# Node Droid
+
+Creates a Standard Template for a Empty NodeJs Project.
+
+## Requirements before we can start
+
+First of all we have to Download and Install VSCode from [here](https://code.visualstudio.com/). After the installation I recommend to install these extensions.
+
+### VSCode Extensions
+
+| Extension                                | Description                                                 |
+|------------------------------------------|-------------------------------------------------------------|
+| ESLint                                   | linting Code on the Fly                                     |
+| Node TDD                                 | Helps to execute Tests when Code is changed                 |
+| Coverage Gutters                         | Show Code Coverage on the Fly                               |
+| Document This                            | Fast and Easy Documentation Generation                      |
+| npm && npm intellisense                  | enables Intellisense in package.json                        |
+| Path Intellisense                        | Intellisense for Paths                                      |
+| Project Manager                          | working with multiple Projects                              |
+| GIT Lens                                 | brings GIT Informations in the Code                         |
+| GraphQl for VSCode                       | brings GraphQl Syntax Highliting and Intellisense to VSCode |
+| Linux Themes for VS Code && vscode-icons | better Styling                                              |
+
+The second dependency is Yarn. This tool builds NPM and allows you to install the packages faster. To install Yarn please follow the instructions [here](https://yarnpkg.com/en/docs/install).
+
+## Install Tool
+
+Now we are ready to install the nodroid command. To do this we only need to execute the following command.
+
+```bash
+yarn global add nodroid
+```
+
+## Usage
+
+```bash
+nodroid init
+
+Name of your Project?:{input name of Project}
+Author?:{input the name of the Author}
+Authors Email?:{input Email of the Author}
+License?:{input License Type}
+```
+
+## Folder Structure
+
+| Folder       | Description                    |
+|--------------|--------------------------------|
+| .vscode      | Settings for VSCode            |
+| node_modules | dependencies                   |
+| spec         | Folder for Tests               |
+| src          | Source Code Folder             |
+| dist         | buildfolder for Release        |
+| coverage     | generated Coverage Folder      |
+| docs         | generated Documentation Folder |
+
+## Scripts
+
+| Script | What it does                         |
+|--------|--------------------------------------|
+| start  | start the index.js in the src Folder |
+| test   | run the Tests and generate Coverage  |
+| build  | build the Code for Release usage     |
+| docs   | generate the Documentation           |
+
+## under the Hood
+
+| Package               | Using for...                                           |
+|-----------------------|--------------------------------------------------------|
+| babel-cli             | use Babel Commands for Transpiling                     |
+| babel-plugin-istanbul | used for generation of Coverage                        |
+| babel-preset-es2015   | transpiling Rules to ES2015                            |
+| babel-preset-stage-2  | transpiling Rules for newest ES Features               |
+| babel-register        | helps to transpile Code for Testing                    |
+| chai                  | assert Testing Tool                                    |
+| mocha                 | Test Runner                                            |
+| sinon                 | Mocking Tool                                           |
+| nyc                   | Coverage Reporter                                      |
+| esdoc                 | Documentation Generator for ES2017                     |
+| esdoc-standard-plugin | the Default Pugin for esdoc                            |
+| eslint                | check Javascript Code for Errors                       |
+| eslint-config-google  | eslint Google Ruleset                                  |
+| nodemon               | used to restart Processes when Source Code has changed |
---
+++ index.js
@@ -0,0 +1,226 @@
+const {mkdirSync, writeFileSync} = require('fs');
+const {join} = require('path');
+const {spawn, spawnSync} = require('child_process');
+const clean = require('rimraf');
+const readline = require('readline');
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const OVERWRITE_UTF8 = {
+    encoding: 'utf-8',
+    flag: 'w',
+};
+const YARN_CONFIG_CACHE = {};
+
+let projectName = '';
+let authorName = '';
+let authorEmail = '';
+let lic = '';
+let root = '';
+
+function createFolders(project) {
+    return new Promise((resolve, reject) => {
+        root = join(process.cwd(), project);
+        clean(root, () => {
+            mkdirSync(root);
+            mkdirSync(join(root, '.vscode'));
+            mkdirSync(join(root, 'spec'));
+            mkdirSync(join(root, 'src'));
+            resolve();
+        });
+    });
+}
+
+function createFiles(contents) {
+    writeFileSync(join(root, '.vscode', 'settings.json'),
+        contents[0], OVERWRITE_UTF8);
+    writeFileSync(join(root, '.esdoc.json'),
+        contents[1], OVERWRITE_UTF8);
+    writeFileSync(join(root, '.eslintrc'),
+        contents[2], OVERWRITE_UTF8);
+    writeFileSync(join(root, '.gitignore'),
+        contents[3], OVERWRITE_UTF8);
+    writeFileSync(join(root, '.npmignore'),
+        contents[4], OVERWRITE_UTF8);
+    writeFileSync(join(root, '.nycrc'),
+        contents[5], OVERWRITE_UTF8);
+    writeFileSync(join(root, 'README.md'),
+        contents[6], OVERWRITE_UTF8);
+    writeFileSync(join(root, 'src', 'index.js'), '', OVERWRITE_UTF8);
+}
+
+function yarnInit() {
+    return new Promise((resolve, reject) => {
+        let yarn = spawn('yarn', ['init', '--yes'], {
+            cwd: root
+        });
+        yarn.stdout.on('data', (d) => {
+            console.info(d.toString('utf-8'));
+        });
+        yarn.stderr.on('data', (d) => {
+            console.info(d.toString('utf-8'));
+        });
+        yarn.on('close', () => {
+            resolve();
+        });
+    });
+}
+
+function yarnInstall() {
+    return new Promise((resolve, reject) => {
+        let yarn = spawn('yarn', [
+            'add', '--dev', 'babel-cli', 'babel-plugin-istanbul', 'babel-preset-es2015', 'babel-preset-stage-2',
+            'babel-register', 'chai', 'esdoc', 'esdoc-standard-plugin', 'eslint', 'eslint-config-google', 'mocha',
+            'nodemon', 'nyc', 'sinon'
+        ], {
+            cwd: root
+        });
+        yarn.stdout.on('data', (d) => {
+            console.info(d.toString('utf-8'));
+        });
+        yarn.stderr.on('data', (d) => {
+            console.info(d.toString('utf-8'));
+        });
+        yarn.on('close', () => {
+            resolve();
+        });
+    });
+}
+
+function updatePackageJson() {
+    let packFile = join(root, 'package.json');
+    let p = require(packFile);
+    p.main = './src/index.js';
+    p.license = lic;
+    p.scripts = {
+        start: 'babel-node ./src/index.js',
+        test: 'NODE_ENV=test babel-node ./node_modules/.bin/nyc ./node_modules/.bin/mocha -R tap --recursive ./spec/',
+        build: 'babel ./src -d ./dist',
+        docs: 'esdoc'
+    };
+    p.author = {
+        name: authorName,
+        email: authorEmail
+    };
+    writeFileSync(packFile, JSON.stringify(p, null, 4), OVERWRITE_UTF8);
+}
+
+function query(msg) {
+    return new Promise((resolve, reject) => {
+        rl.question(msg, resolve);
+    });
+}
+
+async function run() {
+    projectName = await query('Name of your Project?:');
+    authorName = await query('Author?:');
+    authorEmail = await query('Authors Email?:');
+    lic = await query('License?:');
+    if (typeof projectName !== 'string' || projectName.length < 1) {
+        throw Error(`invalid project name ${projectName}`);
+    }
+    console.info(`create Project ${projectName}`);
+
+    const VSCODE_SETTINGS_JSON = `
+{
+    "nodeTdd.activateOnStartup": true,
+    "nodeTdd.coverageThreshold": 85,
+    "nodeTdd.reporter": "tap",
+    "nodeTdd.showCoverage": true,
+    "eslint.autoFixOnSave": true
+}
+    `;
+    const ESDOC_SETTINGS = `
+{
+    "source": "./src",
+    "destination": "./docs",
+    "plugins": [
+        {"name": "esdoc-standard-plugin"}
+    ]
+}
+    `;
+    const ESLINT_SETTINGS = `
+{
+    "globals": {
+        "Promise": true
+    },
+    "env": {
+        "es6": true,
+        "mocha": true,
+        "node": true
+    },
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": "off"
+    }
+}
+    `;
+    const GITIGNORE = `
+.nyc_output/**
+coverage/**
+node_modules/**
+    `;
+    const NPMIGNORE = `
+.nyc_output/**
+.vscode/**
+coverage/**
+node_modules/**
+spec/**
+src/**
+.babelrc
+.esdoc.json
+.gitignore
+.npmignore
+.nycrc
+    `;
+    const NYC_SETTINGS = `
+{
+    "report-dir": "./coverage",
+    "reporter": [
+        "html",
+        "lcov"
+    ],
+    "clean": true,
+    "include": [
+        "src/**/*.js"
+    ],
+    "exclude": [
+        "spec/**/*.spec.js"
+    ],
+    "require": [
+        "babel-register"
+    ],
+    "sourceMap": false,
+    "instrument": true,
+    "all": true
+}
+    `;
+    const README = `
+# ${projectName}
+    `;
+
+    await createFolders(projectName);
+    await yarnInit();
+    await yarnInstall();
+    createFiles([
+        VSCODE_SETTINGS_JSON,
+        ESDOC_SETTINGS,
+        ESLINT_SETTINGS,
+        GITIGNORE,
+        NPMIGNORE,
+        NYC_SETTINGS,
+        README,
+    ]);
+    updatePackageJson();
+}
+
+run()
+.then(process.exit)
+.catch(console.error);
